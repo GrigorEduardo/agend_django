e1bb030e542376c09163c5861599d4c5355d0008
GrigorEduardo <gromioresponde27@gmail.com>
2024-12-18 00:01:51 -0300

    adicionando algumas att

---
+++ contact/views/contact_views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
+
+
+# Create your views here.
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    context = {
+        'contacts': contacts,
+    }
+    return render(
+        request, 
+        'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+
+    single_contact = get_object_or_404(Contact, pk=contact_id)
+    
+    context = {
+        'contact': single_contact,
+    }
+    print(single_contact)
+    return render(
+        request, 
+        'contact/contact.html',
+        context
+    )
